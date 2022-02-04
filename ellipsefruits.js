009938372219d7651c34362d507b47b7594b1ad5
zhayun3233 <76890077+zhayun3233@users.noreply.github.com>
2022-02-04 17:23:43 -0500

    Add files via upload

---
+++ ellipsefruits.js
@@ -0,0 +1,87 @@
+//My First Project 2012 Summer
+//Ellipse Fruits
+//Graphic Experiment
+//background
+fill(61, 25, 4);
+ellipse(300, 200, 1500, 450);
+
+//tomato on a stick
+fill(255, 0, 0);
+ellipse(30, 25, 100, 85);
+
+//leaf on tomato
+fill(20, 212, 36);
+ellipse(30, 10, 60, 10);  
+
+//stick
+fill(240, 144, 60);
+ellipse(15, 75, 15, 80);
+
+//blueberry
+fill(20, 36, 179);
+ellipse(90, 15, 15, 20);
+
+//grape
+fill(222, 11, 39);
+ellipse(120, 20, 20, 30);
+
+//apple
+fill(217, 19, 15);
+ellipse(320, 50 , 75, 75);
+
+//stem
+fill(219, 141, 81);
+ellipse(320, 18, 5, 30);
+
+//leaf
+fill(58, 184, 44);
+ellipse(330, 15, 30, 5);
+    
+//coconut
+fill(138, 71, 33);
+ellipse(190, 90, 150, 150);
+
+//dots
+fill(5, 2, 2);
+ellipse(190, 45, 15, 15);
+ellipse(200, 30, 15, 15);
+ellipse(180, 30, 15, 15);
+
+//orange
+fill(255, 89, 0);
+ellipse(70, 125, 90, 90);
+
+//watermelon
+fill(36, 107, 26);
+ellipse(150, 275, 250, 200);
+
+//stripes
+fill(43, 255, 0);
+stroke(43, 255, 0);
+ellipse(148, 250, 240, 30);
+ellipse(145, 186, 100, 20);
+ellipse(150, 310, 235, 25);
+ellipse(150, 365, 100, 15);
+
+//pear
+fill(77, 232, 16);
+stroke(77, 237, 14);
+ellipse(320, 140, 50, 50);
+ellipse(320, 160, 60, 60);
+
+//pear stem
+stroke(5, 0, 0);
+fill(219, 141, 81);
+ellipse(320,110, 5, 30);
+
+//lemon
+fill(242, 222, 8);
+ellipse(330, 230, 75, 50);
+
+//lime
+fill(12, 163, 12);
+ellipse(330, 300, 75, 50);
+
+//kiwi
+fill(138, 71, 33);
+ellipse(330, 370, 75, 50);
